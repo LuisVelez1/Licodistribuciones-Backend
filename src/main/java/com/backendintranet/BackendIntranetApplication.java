93ca63e7741b2608802fb5f37f489b45870d4b68
Luis Velez <luiseduardovelez881@gmail.com>
2026-03-01 21:20:54 -0500

    Add backend 1.0.0

---
+++ src/main/java/com/backendintranet/BackendIntranetApplication.java
@@ -0,0 +1,13 @@
+package com.backendintranet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendIntranetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendIntranetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendintranet/BackendIntranetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendintranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIntranetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
